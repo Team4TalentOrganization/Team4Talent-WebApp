3ffb6e4f874d76e425b7dc8e3f3532223c97ff15
DriesCoxPXL <dries.cox@student.pxl.be>
2023-11-23 15:34:24 +0100

    test contactpagina toegevoegd

---
+++ StudyGuidance/StudyGuidance.Web.Tests/ContactPageTests.cs
@@ -0,0 +1,23 @@
+﻿using StudyGuidance.Web.Pages;
+
+namespace StudyGuidance.Web.Tests
+{
+    public class ContactPageTests
+    {
+        [Test]
+        public void ContactFormRendersCorrectly()
+        {
+            // Arrange
+            using var ctx = new Bunit.TestContext();
+            var cut = ctx.RenderComponent<ContactPage>(); 
+
+            // Act - geen actie nodig omdat we alleen de rendering testen
+
+            // Assert
+            Assert.NotNull(cut.Find("form")); 
+            Assert.NotNull(cut.Find("input#inputName")); 
+            Assert.NotNull(cut.Find("input#inputMessage[type=email]")); 
+            Assert.NotNull(cut.Find("button[type=submit]")); 
+        }
+    }
+}
