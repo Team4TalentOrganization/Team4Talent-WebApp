45c6408c3614cf98a180ea84dcb07f30f0e8c65e
LucaHendrickxPXL <93264347+LucaHendrickxPXL@users.noreply.github.com>
2023-12-01 14:29:57 +0100

    QuizClient gemaakt

--- StudyGuidance/StudyGuidance.Web/ApiClient/QuizApiClient.cs
+++ StudyGuidance/StudyGuidance.Web/ApiClient/QuizApiClient.cs
@@ -1,6 +1,18 @@
-﻿namespace StudyGuidance.Web.ApiClient
+﻿using StudyGuidance.Web.Models;
+
+namespace StudyGuidance.Web.ApiClient
 {
-    public class QuizApiClient
+    public class QuizApiClient : BaseApiClient, IQuizApiClient
     {
+        private string _url = $"https://localhost:7109/api/Quiz";
+
+        public QuizApiClient(HttpClient httpClient, ILogger<BaseApiClient> logger) : base(httpClient, logger)
+        {
+        }
+
+        public async Task<List<Question>> GetAllQuestion()
+        {
+            return await GetJsonAsync<List<Question>>($"{_url}/questions");
+        }
     }
 }
