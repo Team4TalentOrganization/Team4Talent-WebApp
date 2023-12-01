4c4117d9261831b7d44aa1e5f091b3442abe4d85
LucaHendrickxPXL <93264347+LucaHendrickxPXL@users.noreply.github.com>
2023-12-01 15:40:56 +0100

    testen geschreven

---
+++ StudyGuidance/StudyGuidance.Web.Tests/Models/OptionTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using StudyGuidance.Web.Models;
+
+namespace StudyGuidance.Tests.Models
+{
+    public class OptionTests
+    {
+        [Test]
+        public void OptionProperties_DefaultValues_ShouldBeInitialized()
+        {
+            var option = new Option();
+
+            Assert.IsNull(option.Content);
+            Assert.AreEqual(0, option.OptionRelation);
+            Assert.AreEqual(0, option.QuestionId);
+            Assert.AreEqual(0, option.OptionId);
+        }
+
+        [Test]
+        public void OptionProperties_SetValues_ShouldReturnCorrectValues()
+        {
+            var option = new Option
+            {
+                Content = "A",
+                OptionRelation = 1,
+                QuestionId = 123,
+                OptionId = 456
+            };
+
+            // Assert
+            Assert.AreEqual("A", option.Content);
+            Assert.AreEqual(1, option.OptionRelation);
+            Assert.AreEqual(123, option.QuestionId);
+            Assert.AreEqual(456, option.OptionId);
+        }
+
+        [Test]
+        public void OptionEquality_DifferentProperties_ShouldNotBeEqual()
+        {
+            var option1 = new Option
+            {
+                Content = "A",
+                OptionRelation = 1,
+                QuestionId = 123,
+                OptionId = 456
+            };
+
+            var option2 = new Option
+            {
+                Content = "B",
+                OptionRelation = 2,
+                QuestionId = 789,
+                OptionId = 101
+            };
+
+            Assert.AreNotEqual(option1, option2);
+        }
+    }
+}
+
---
+++ StudyGuidance/StudyGuidance.Web.Tests/Models/QuestionTests.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using StudyGuidance.Web.Models;
+using System.Collections.Generic;
+
+namespace StudyGuidance.Tests.Models
+{
+    public class QuestionTests
+    {
+        [Test]
+        public void QuestionProperties_DefaultValues_ShouldBeInitialized()
+        {
+            var question = new Question();
+
+            Assert.AreEqual(0, question.QuestionId);
+            Assert.IsNull(question.Phrase);
+        }
+
+        [Test]
+        public void QuestionProperties_SetValues_ShouldReturnCorrectValues()
+        {
+            var options = new List<Option>
+            {
+                new Option { Content = "A", OptionRelation = 1, QuestionId = 123, OptionId = 456 },
+                new Option { Content = "B", OptionRelation = 2, QuestionId = 123, OptionId = 457 }
+            };
+
+            var question = new Question
+            {
+                Options = options,
+                QuestionId = 123,
+                Phrase = "Choose the correct option."
+            };
+
+            Assert.AreEqual(options, question.Options);
+            Assert.AreEqual(123, question.QuestionId);
+            Assert.AreEqual("Choose the correct option.", question.Phrase);
+        }
+
+        
+        [Test]
+        public void QuestionEquality_DifferentProperties_ShouldNotBeEqual()
+        {
+            var options1 = new List<Option>
+            {
+                new Option { Content = "A", OptionRelation = 1, QuestionId = 123, OptionId = 456 },
+                new Option { Content = "B", OptionRelation = 2, QuestionId = 123, OptionId = 457 }
+            };
+
+            var options2 = new List<Option>
+            {
+                new Option { Content = "C", OptionRelation = 3, QuestionId = 789, OptionId = 101 },
+                new Option { Content = "D", OptionRelation = 4, QuestionId = 789, OptionId = 102 }
+            };
+
+            var question1 = new Question
+            {
+                Options = options1,
+                QuestionId = 123,
+                Phrase = "Choose the correct option."
+            };
+
+            var question2 = new Question
+            {
+                Options = options2,
+                QuestionId = 789,
+                Phrase = "Choose another option."
+            };
+
+            Assert.AreNotEqual(question1, question2);
+        }
+    }
+}
+
