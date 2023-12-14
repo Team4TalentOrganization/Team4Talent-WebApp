6c7ec1b0a0264c6ddacb0687ad5faa30b2a1f210
yomaxx <max.valkenburg@hotmail.com>
2023-12-14 09:05:35 +0100

    trigger selenium veranderen

--- StudyGuidance/StudGuidance.Api/Program.cs
+++ StudyGuidance/StudGuidance.Api/Program.cs
@@ -27,10 +27,11 @@ builder.Services.AddScoped<IQuestionModificationService, QuestionModificationSer
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
-
+}
 
 app.UseHttpsRedirection();
 
