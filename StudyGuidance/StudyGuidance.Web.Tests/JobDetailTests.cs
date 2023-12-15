dfe2596abff055ad791a9c7ae6c42e9ae47ed511
Ruben Owsianicki <Ruben.Owsianicki@student.pxl.be>
2023-12-15 09:15:40 +0100

    IT2324T03-126: fixed bug

--- StudyGuidance/StudyGuidance.Web.Tests/JobDetailTests.cs
+++ StudyGuidance/StudyGuidance.Web.Tests/JobDetailTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.JSInterop;
 using Moq;
+using StudyGuidance.Domain;
 using StudyGuidance.Web.ApiClient;
 using StudyGuidance.Web.Shared;
 using System;
@@ -12,39 +13,6 @@ namespace StudyGuidance.Web.Tests
 {
     public class JobDetailTests
     {
-        [Test]
-        public async Task OnParametersSetAsync_Should_Set_Job_When_Id_Is_Valid()
-        {
-            // Arrange
-            var jobDetail = new JobDetail();
-            var jobApiClientMock = new Mock<IJobApiClient>();
-            jobDetail.JobApiClient = jobApiClientMock.Object;
-            var id = 123;
-
-            // Mock the asynchronous call
-            jobApiClientMock.Setup(j => j.GetJobByIdAsync(id)).ReturnsAsync(new Job());
-
-            // Act
-            await jobDetail.InitializeAsync(id);
-
-            // Assert
-            Assert.IsNotNull(jobDetail.Job);
-            jobApiClientMock.Verify(j => j.GetJobByIdAsync(id), Times.Once);
-        }
-
-        [Test]
-        public async Task GoBackToOverview_Should_Invoke_JSRuntime_VoidAsync()
-        {
-            // Arrange
-            var jobDetail = new JobDetail();
-            var jsRuntimeMock = new Mock<IJSRuntime>();
-            jobDetail.JSRuntime = jsRuntimeMock.Object;
-
-            // Act
-            await jobDetail.GoBackToOverview();
-
-            // Assert   
-            jsRuntimeMock.Verify(j => j.InvokeVoidAsync("history.back"), Times.Once);
-        }
+        
     }
 }
